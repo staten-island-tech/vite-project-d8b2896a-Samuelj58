0d2ba5225e1d5d8ca208583dd580c93f6a37345f
Samuelj58 <144460795+Samuelj58@users.noreply.github.com>
2023-11-09 10:03:30 -0500

    ww

---
+++ menu.js
@@ -0,0 +1,26 @@
+const Food = [
+    {
+        name: "Tomato Soup",
+        meat: false,
+        price: 4.0,
+        color: "Red",
+        image: "tsoup.jpg",
+    },
+    {
+        name: "Miso Soup",
+        meat: false,
+        price: 4.0,
+        color: "Yellow",
+        image: "msoup.jpg",
+    },
+    {
+        name: "Potato Beer Cheese Soup",
+        meat: false,
+        price: 5.0,
+        color: "Yellow",
+        image: "pbcsoup.jpg",
+    },
+    {
+        
+    }
+]
