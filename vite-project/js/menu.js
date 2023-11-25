cd6adb3db562314fc742dbb7f88e13a19baa1a97
Samuelj58 <Jiangsamuel11@gmail.com>
2023-11-24 21:51:23 -0500

    ww

---
+++ vite-project/js/menu.js
@@ -0,0 +1,105 @@
+const Menu = [
+    {
+      name: 'Spaghetti Bolognese',
+      hasMeat: true,
+      price: 12.99,
+      image: 'https://example.com/images/spaghetti-bolognese.jpg'
+    },
+    {
+      name: 'Vegetable Stir-Fry',
+      hasMeat: false,
+      price: 9.99,
+      image: 'https://example.com/images/vegetable-stir-fry.jpg'
+    },
+    {
+      name: 'Roast Chicken',
+      hasMeat: true,
+      price: 14.50,
+      image: 'https://example.com/images/roast-chicken.jpg'
+    },
+    {
+      name: 'Margherita Pizza',
+      hasMeat: false,
+      price: 11.00,
+      image: 'https://example.com/images/margherita-pizza.jpg'
+    },
+    {
+      name: 'Beef Stroganoff',
+      hasMeat: true,
+      price: 15.99,
+      image: 'https://example.com/images/beef-stroganoff.jpg'
+    },
+    {
+      name: 'Mushroom Risotto',
+      hasMeat: false,
+      price: 10.50,
+      image: 'https://example.com/images/mushroom-risotto.jpg'
+    },
+    {
+      name: 'Barbecue Ribs',
+      hasMeat: true,
+      price: 18.75,
+      image: 'https://example.com/images/barbecue-ribs.jpg'
+    },
+    {
+      name: 'Caprese Salad',
+      hasMeat: false,
+      price: 8.50,
+      image: 'https://example.com/images/caprese-salad.jpg'
+    },
+    {
+      name: 'Sausage Rolls',
+      hasMeat: true,
+      price: 7.99,
+      image: 'https://example.com/images/sausage-rolls.jpg'
+    },
+    {
+      name: 'Pesto Pasta',
+      hasMeat: false,
+      price: 9.25,
+      image: 'https://example.com/images/pesto-pasta.jpg'
+    },
+    {
+      name: 'Beef Burger',
+      hasMeat: true,
+      price: 13.50,
+      image: 'https://example.com/images/beef-burger.jpg'
+    },
+    {
+      name: 'Falafel Wrap',
+      hasMeat: false,
+      price: 8.75,
+      image: 'https://example.com/images/falafel-wrap.jpg'
+    },
+    {
+      name: 'Pulled Pork Sandwich',
+      hasMeat: true,
+      price: 11.99,
+      link: 'https://example.com/pulled-pork-sandwich',
+      image: 'https://example.com/images/pulled-pork-sandwich.jpg'
+    },
+    {
+      name: 'Vegetarian Sushi',
+      hasMeat: false,
+      price: 12.00,
+      image: 'https://example.com/images/vegetarian-sushi.jpg'
+    },
+    {
+      name: 'Beef Tacos',
+      hasMeat: true,
+      price: 10.25,
+      image: 'https://example.com/images/beef-tacos.jpg'
+    },
+    {
+      name: 'Vegetable Lasagna',
+      hasMeat: false,
+      price: 13.75,
+      image: 'https://example.com/images/vegetable-lasagna.jpg'
+    },
+    {
+      name: 'Lamb Kebabs',
+      hasMeat: true,
+      price: 16.50,
+      image: 'https://example.com/images/lamb-kebabs.jpg'
+    },
+]
